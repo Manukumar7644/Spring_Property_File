67cf1f53b67c2cabab645b7628f82d8824b03bab
Manu Kumar <143504163+Manukumar7644@users.noreply.github.com>
2026-05-10 01:07:03 +0530

    Add files via upload

---
+++ PropertyFileEx-2/src/in/scalive/beans/Student.java
@@ -0,0 +1,25 @@
+package in.scalive.beans;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	
+	
+	private int roll;	
+	
+	
+    private String name;    
+    public Student(@Value("${student.roll}")int roll,@Value("${student.name}")String name) {
+    	this.roll=roll;
+    	this.name=name;
+    	System.out.println("Student bean created...");
+    }
+    
+	
+	public void show() {
+    	System.out.println("Name:"+name);
+    	System.out.println("Roll="+roll);
+    }
+}
